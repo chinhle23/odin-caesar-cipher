a1ada450fd8dcf33e5447a8c2995da97f8a858e7
chinhle23 <112718475+chinhle23@users.noreply.github.com>
2024-06-08 03:21:49 -0400

    docs: change filename to snake_case

---
+++ caesar_cipher.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+def caesar_cipher(string, number)
+  alphabet = []
+  ciphered_arr = []
+  ('a'..'z').each { |letter| alphabet.push(letter) }
+  string_arr = string.split('')
+  string_arr.each do |char|
+    ciphered_arr.push(alphabet.include?(char.downcase) ? if char.downcase == char
+                                                           alphabet[alphabet.index(char.downcase) + number - alphabet.length]
+                                                         else
+                                                           alphabet[alphabet.index(char.downcase) + number - alphabet.length].upcase
+                                                         end : char)
+  end
+  ciphered_arr.join('')
+end
+
+p caesar_cipher('What a string!', 5) # "Bmfy f xywnsl!"
